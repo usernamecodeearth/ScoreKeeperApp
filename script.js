4673f00aa15e106f35034267bfc22848061e57ee
Mary <maryastevens20@gmail.com>
2021-12-13 20:11:45 -0500

    refractoring of p1 and p2 buttons/scores

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-const p1Score = document.querySelector('#p1');
-const p2Score = document.querySelector('#p2');
-const p1AddBtn = document.querySelector('#addP1Btn');
-const p2AddBtn = document.querySelector('#addP2Btn');
+const p1 = {
+  score: document.querySelector('#p1'),
+  button: document.querySelector('#addP1Btn'),
+}
+
+const p2 = {
+  score: document.querySelector('#p2'),
+  button: document.querySelector('#addP2Btn'),
+}
+
+//const p1Score = document.querySelector('#p1');
+//const p2Score = document.querySelector('#p2');
+//const p1AddBtn = document.querySelector('#addP1Btn');
+//const p2AddBtn = document.querySelector('#addP2Btn');
 const resetBtn = document.querySelector('#resetBtn')
 const endScore = document.querySelector('#endScore')
+
+function scores(player, opponent) {
+  player.button.disabled = true;
+  opponent.button.disabled = true;
+  player.score.classList.add('win')
+  opponent.score.classList.add('lose')
+}
+
 
 let winningNumber = 5;
 
 endScore.addEventListener('change', () => {
-  p2Score.innerHTML = 0;
-  p1Score.innerHTML = 0;
+  p2.score.innerHTML = 0;
+  p1.score.innerHTML = 0;
   winningNumber = endScore.value
 })
 
-p1AddBtn.addEventListener('click', () => {
-  p1Score.innerHTML++;
-  if (parseInt(p1Score.innerHTML) == winningNumber) {
-    p1AddBtn.disabled = true;
-    p2AddBtn.disabled = true;
-    p1Score.classList.add('win')
-    p2Score.classList.add('lose')
-    
+p1.button.addEventListener('click', () => {
+  p1.score.innerHTML++;
+  if (parseInt(p1.score.innerHTML) == winningNumber) {
+    scores(p1, p2)
   }
 })
 
-p2AddBtn.addEventListener('click', () => {
-  p2Score.innerHTML++;
-  if (parseInt(p2Score.innerHTML) == winningNumber) {
-    p1AddBtn.disabled = true;
-    p2AddBtn.disabled = true;
-    p2Score.classList.add('win')
-    p1Score.classList.add('lose')
-    
+p2.button.addEventListener('click', () => {
+  p2.score.innerHTML++;
+  if (parseInt(p2.score.innerHTML) == winningNumber) {
+    scores(p2, p1)
   }
 })
 
 resetBtn.addEventListener('click', () => {
-  p2Score.innerHTML = 0;
-  p1Score.innerHTML = 0;
-  p1AddBtn.disabled = false;
-  p2AddBtn.disabled = false;
-  p1Score.classList.remove('win', 'lose')
-  p2Score.classList.remove('win', 'lose')
+  p2.score.innerHTML = 0;
+  p1.score.innerHTML = 0;
+  p1.button.disabled = false;
+  p2.button.disabled = false;
+  p1.score.classList.remove('win', 'lose')
+  p2.score.classList.remove('win', 'lose')
 })
